8a0654c52d2350e0f0eabc5403a46f91e839e57e
Mike Bedoya <mikebedoya28@gmail.com>
2024-08-19 19:19:35 +0000

    NavBar1correct

--- src/js/component/Footer01.jsx
+++ src/js/component/Footer01.jsx
@@ -2,10 +2,10 @@ import React from "react";
 export const Footer01 = () => {
 
   return (
-    <div class="container">
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <span class="mb-3 mb-md-0 text-body-secondary">Copyright @yourwebsite 2024</span>
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div className="col-md-4 d-flex align-items-center">
+        <span className="mb-3 mb-md-0 text-body-secondary">Copyright @yourwebsite 2024</span>
       </div>
       </footer>
     </div>
