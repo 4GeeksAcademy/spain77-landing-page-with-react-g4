e393fd28cdb09485382917f85a62eb5086932bc9
Mike Bedoya <mikebedoya28@gmail.com>
2024-08-19 18:42:42 +0000

    Footer01

---
+++ src/js/component/Footer01.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export const Footer01 = () => {
+
+  return (
+    <div className="card ">
+      <div className="card-body">
+        <h5 className="card-title">Copiright @yourwebsite 2024</h5>
+      </div>
+    </div>
+
+  )
+
+}
