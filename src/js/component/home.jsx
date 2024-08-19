da11c6592c7378c80caba2fab1869da9d402c664
Joni Santos <jonisantos1996@hotmail.com>
2024-08-19 18:44:41 +0000

    feat: cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "./Card.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex">
+			<Card title="Adaptability" description="Adaptability is the ability to adjust and thrive in changing circumstances, and it is essential to navigating the complexities of life." />
+			<Card title="Humility" description="Humility is the practice of recognizing our limitations and imperfections, and it is essential to personal growth and development." />
+			<Card title="Conservation Tools" description="One of the most important uses of technology in nature is the development of conservation tools." />
+			<Card title="Honesty" description="Honesty is the practice of speaking and acting truthfully and with integrity, and it is essential to building trust and respect in our relationships." />
 		</div>
 	);
 };
