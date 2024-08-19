5643bc0cb76c2ee2f58b20ab517a255901bb2c34
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-08-19 18:15:37 +0000

    feat:header

---
+++ src/js/component/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () =>{
+    return (
+        <div className="container">
+            <h1>A Warm Welcome!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic dolores obcaecati dolorem, necessitatibus quasi magnam non veritatis qui officia nobis eos incidunt impedit minima velit ab adipisci ea quaerat vitae.</p>
+            <a href="#" className="btn btn-primary btn-lg">Call to action!</a>
+        </div>
+    );
+} 
