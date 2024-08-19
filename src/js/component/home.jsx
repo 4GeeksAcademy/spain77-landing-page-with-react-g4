5471175f99defa1baed54bd4db51591ee67b80a3
Joni Santos <jonisantos1996@hotmail.com>
2024-08-19 19:03:45 +0000

    fix: footer

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,13 +11,13 @@ const Home = () => {
 	return (
 		<div className="container">
 			<Header />
-    <div className="d-flex">
-			<Card title="Adaptability" description="Adaptability is the ability to adjust and thrive in changing circumstances, and it is essential to navigating the complexities of life." />
-			<Card title="Humility" description="Humility is the practice of recognizing our limitations and imperfections, and it is essential to personal growth and development." />
-			<Card title="Conservation Tools" description="One of the most important uses of technology in nature is the development of conservation tools." />
-			<Card title="Honesty" description="Honesty is the practice of speaking and acting truthfully and with integrity, and it is essential to building trust and respect in our relationships." />
-		</div>
-      <Footer01 />
+    		<div className="d-flex">
+				<Card title="Adaptability" description="Adaptability is the ability to adjust and thrive in changing circumstances, and it is essential to navigating the complexities of life." />
+				<Card title="Humility" description="Humility is the practice of recognizing our limitations and imperfections, and it is essential to personal growth and development." />
+				<Card title="Conservation Tools" description="One of the most important uses of technology in nature is the development of conservation tools." />
+				<Card title="Honesty" description="Honesty is the practice of speaking and acting truthfully and with integrity, and it is essential to building trust and respect in our relationships." />
+			</div>
+      		<Footer01 />
 		</div>
 	);
 };
